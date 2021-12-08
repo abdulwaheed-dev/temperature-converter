c984664ca563bcc7e6ecb2169e9595d7dc498f47
Abdul_Waheed <abdul.bcss19@iba-suk.edu.pk>
2021-12-08 12:25:31 +0500

    roundoff-functionality added

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ let farhInp = document.querySelector("#farTemp");
 celInp.addEventListener("input",function(){
         let celVal = this.value;
         let farhVal = ((celVal * 9/5)+32);
+        if(!Number.isInteger(farhVal)){
+                farhVal = farhVal.toFixed(4);
+        }
         farhInp.value = farhVal;
 });
     farhInp.addEventListener("input",function(){
         let farhVal = this.value;
         let celVal = ((farhVal-32) * 5/9); 
+        if(!Number.isInteger(celVal)){
+                celVal = celVal.toFixed(4);
+        }
         celInp.value = celVal;
 });
